149a9a0d66780d13c5601a6c7e024820b35f68b4
Johnny <jonathanbc2012@gmail.com>
2024-12-03 20:14:48 +0100

    first commit

---
+++ src/main/java/com/johnnybcode/projectmanagementsystem/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.johnnybcode.projectmanagementsystem.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "qpwoeiru mzvbzmvbgaksjghtweuit zxcvmzx kasdfkhtitylg";
+    public static final String JWT_HEADER = "Authorization";
+}
