09525283561037d510296afe0942f7c427f29ba8
Johnny <jonathanbc2012@gmail.com>
2024-12-04 20:12:35 +0100

    Chat Repository and ChatServiceImpl created

---
+++ src/main/java/com/johnnybcode/projectmanagementsystem/service/ChatServiceImpl.java
@@ -0,0 +1,18 @@
+package com.johnnybcode.projectmanagementsystem.service;
+
+import com.johnnybcode.projectmanagementsystem.model.Chat;
+import com.johnnybcode.projectmanagementsystem.repository.ChatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatServiceImpl implements ChatService{
+
+    @Autowired
+    private ChatRepository chatRepository;
+
+    @Override
+    public Chat createChat(Chat chat) {
+        return chatRepository.save(chat);
+    }
+}
