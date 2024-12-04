3712f7b0895283fce183dac3bd7e036ebb47568f
Johnny <jonathanbc2012@gmail.com>
2024-12-04 20:01:22 +0100

    feat: UserServiceImpl methods created

---
+++ src/main/java/com/johnnybcode/projectmanagementsystem/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.johnnybcode.projectmanagementsystem.service;
+
+import com.johnnybcode.projectmanagementsystem.config.JwtProvider;
+import com.johnnybcode.projectmanagementsystem.model.User;
+import com.johnnybcode.projectmanagementsystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findUserProfileByJwt(String jwt) throws Exception {
+        String email = JwtProvider.getEmailFromToken(jwt);
+
+        return findUserByEmail(email);
+    }
+
+    @Override
+    public User findUserByEmail(String email) throws Exception {
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new Exception("Usuario no encontrado");
+        }
+        return user;
+    }
+
+    @Override
+    public User findUserById(Long userId) throws Exception {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isEmpty()) {
+            throw new Exception("Usuario no encontrado");
+        }
+        return optionalUser.get();
+    }
+
+    @Override
+    public User updateUsersProjectSize(User user, int number) throws Exception {
+        user.setProjectSize(user.getProjectSize() + number);
+
+        return userRepository.save(user);
+    }
+}
