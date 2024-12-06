295401f1a1c8e188235ddbaa69eaad084058ae35
Johnny <jonathanbc2012@gmail.com>
2024-12-06 12:40:31 +0100

    feat: methods deleteProject, updateProject, addUserToProject, removeUserFromProject, getChatByProjectId and searcProjects added to ProjectServiceImpl

--- src/main/java/com/johnnybcode/projectmanagementsystem/service/ProjectServiceImpl.java
+++ src/main/java/com/johnnybcode/projectmanagementsystem/service/ProjectServiceImpl.java
@@ -74,26 +74,62 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public void deleteProject(Long projectId, Long userId) throws Exception {
-
+        getProjectById(projectId);
+        //userService.findUserById(userId);
+        projectRepository.deleteById(projectId);
     }
 
     @Override
-    public Project updateProject(Project updatedProject, Long id) throws Exception {
-        return null;
+    public Project updateProject(Project updatedProject, Long projectId) throws Exception {
+        Project project = getProjectById(projectId);
+
+        project.setName(updatedProject.getName());
+        project.setDescription(updatedProject.getDescription());
+        project.setTags(updatedProject.getTags());
+
+        return projectRepository.save(project);
     }
 
     @Override
     public void addUserToProject(Long projectId, Long userId) throws Exception {
+        Project project = getProjectById(projectId);
+        User user=userService.findUserById(userId);
 
+        if(!project.getTeam().contains(user)){
+            project.getChat().getUsers().add(user);
+            project.getTeam().add(user);
+        }
+
+        projectRepository.save(project);
     }
 
     @Override
     public void removeUserFromProject(Long projectId, Long userId) throws Exception {
+        Project project = getProjectById(projectId);
+        User user=userService.findUserById(userId);
 
+        if(project.getTeam().contains(user)){
+            project.getChat().getUsers().remove(user);
+            project.getTeam().remove(user);
+        }
+
+        projectRepository.save(project);
     }
 
     @Override
     public Chat getChatByProjectId(Long projectId) throws Exception {
-        return null;
+        Project project = getProjectById(projectId);
+
+        return project.getChat();
     }
+
+    @Override
+    public List<Project> searchProjects(String keyword, User user) throws Exception {
+
+        List<Project> projects = projectRepository.findByNameContainingAndTeamContains(keyword, user);
+
+        return projects;
+    }
+
+
 }
