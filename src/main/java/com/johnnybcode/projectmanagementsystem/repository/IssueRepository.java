c34ebef37f7f049446e2e0e4e822738253524131
Johnny <jonathanbc2012@gmail.com>
2024-12-09 19:29:14 +0100

    feat: IssueServiceImpl created

--- src/main/java/com/johnnybcode/projectmanagementsystem/repository/IssueRepository.java
+++ src/main/java/com/johnnybcode/projectmanagementsystem/repository/IssueRepository.java
@@ -4,6 +4,10 @@ import com.johnnybcode.projectmanagementsystem.model.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+
+    public List<Issue> findByProjectId(Long id);
 }
--- src/main/java/com/johnnybcode/projectmanagementsystem/service/IssueService.java
+++ src/main/java/com/johnnybcode/projectmanagementsystem/service/IssueService.java
@@ -9,15 +9,13 @@ import java.util.Optional;
 
 public interface IssueService {
 
-    Optional<Issue> getIssueById(Long issueId) throws Exception;
+    Issue getIssueById(Long issueId) throws Exception;
 
     List<Issue> getIssueByProjectId(Long projectId) throws Exception;
 
-    Issue createIssue(IssueRequest issue, Long userId) throws Exception;
+    Issue createIssue(IssueRequest issueRequest, User user) throws Exception;
 
-    Optional<Issue> updateIssue(Long issueId, IssueRequest updatedIssue, Long userId) throws Exception;
-
-    String deleteIssue(Long issueId, Long userId) throws Exception;
+    void deleteIssue(Long issueId, Long userId) throws Exception;
 
     Issue addUserToIssue(Long issueId, Long userId) throws Exception;
 
