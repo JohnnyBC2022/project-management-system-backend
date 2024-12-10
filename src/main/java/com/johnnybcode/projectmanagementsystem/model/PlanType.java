695c1688e256d0d1840233fc97ad84e8322b2aa5
Johnny <jonathanbc2012@gmail.com>
2024-12-10 19:59:10 +0100

    feat: Subscription model created and PlanType Enum created

---
+++ src/main/java/com/johnnybcode/projectmanagementsystem/model/PlanType.java
@@ -0,0 +1,4 @@
+package com.johnnybcode.projectmanagementsystem.model;
+
+public enum PlanType {
+}
---
+++ src/main/java/com/johnnybcode/projectmanagementsystem/model/Subscription.java
@@ -0,0 +1,4 @@
+package com.johnnybcode.projectmanagementsystem.model;
+
+public class Subscription {
+}
